38a658c9ce67328bc4eb205491c4aeaa0a771b55
lennonwoo <cccmorin@gmail.com>
2019-07-15 19:44:16 -0700

    use TO_NATIVE_PATH to get platform path

--- ports/ros-dashing-ament-cmake-core/portfile.cmake
+++ ports/ros-dashing-ament-cmake-core/portfile.cmake
@@ -10,14 +10,17 @@ vcpkg_from_github(
     SHA512 cf675392b663e84e144e851956ec58936ad85a0ca7d661d95c4429fa4e171767649bb3bbab7f51b21feae05a9bd14647ee377f62daacfc5734f59fe1b6c1302c
 )
 
+set(ROS_BASE_PATH "C:/opt/ros/dashing")
+file(TO_NATIVE_PATH "${ROS_BASE_PATH}" ROS_BASE_PATH)
+
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-        -DCMAKE_INSTALL_PREFIX=c:\\opt\\rosdashing
-        -DCMAKE_PREFIX_PATH=c:\\opt\\rosdashing
+        -DCMAKE_INSTALL_PREFIX=${ROS_BASE_PATH}
+        -DAMENT_PREFIX_PATH=${ROS_BASE_PATH}
 )
 
 vcpkg_install_cmake()
 
-file(INSTALL ${SOURCE_PATH}/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ros-dashing-ament-cmake-core RENAME copyright)
-file(INSTALL ${SOURCE_PATH}/include/ros-dashing-ament-cmake-core_for_vcpkg.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+# file(INSTALL ${SOURCE_PATH}/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/ros-dashing-ament-cmake-core RENAME copyright)
+# file(INSTALL ${SOURCE_PATH}/include/ros-dashing-ament-cmake-core_for_vcpkg.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
